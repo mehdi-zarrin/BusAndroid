a82c70277a990699b056e71bb999d99dd251475a
Mehdi Zarrin <mehdi.zarrin@gmail.com>
2016-05-29 16:13:45 +0430

    add toast message to main activity

--- app/src/main/java/com/example/mehdi/busandroid/MainActivity.java
+++ app/src/main/java/com/example/mehdi/busandroid/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.mehdi.busandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.squareup.otto.Subscribe;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        EventBus.getBus().register(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        EventBus.getBus().unregister(this);
+    }
+
+    @Subscribe
+    public void onMainActivityGet(MessageEvent event) {
+        Toast.makeText(this, "got the message " + event.getMessage(), Toast.LENGTH_LONG).show();
     }
 }
